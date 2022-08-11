5968420c4f8c94c853779acaef6416c943a0a772
Karol Podsiadlo <karolekk131@gmail.com>
2022-08-11 21:26:38 +0200

    delete task by id

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -11,5 +11,6 @@ import java.util.Optional;
 public interface TaskRepository extends CrudRepository<Task,Long> {
     List<Task> findAll();
 
+    @Override
     Optional<Task> findById(Long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,15 @@ public class DbService {
         return taskRepository.findAll();
     }
 
-    public Optional<Task> getTaskById(final Long id){
-        return taskRepository.findById(id);
+    public Task getTaskById(final Long id) throws TaskNotFoundException {
+        return taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task){
+        return taskRepository.save(task);
+    }
+
+    public void deleteTaskById(final Long id){
+        taskRepository.deleteById(id);
     }
 }
