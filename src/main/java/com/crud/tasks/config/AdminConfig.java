f45bab97206f9a64e44a154f6ecb0ebdec33ea44
Karol Podsiadlo <karolekk131@gmail.com>
2022-11-12 21:02:23 +0100

    32.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -9,4 +9,19 @@ import org.springframework.stereotype.Component;
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.app.description}")
+    private String desc;
+
+    @Value("${info.company.goal}")
+    private String goal;
+
+    @Value("${info.company.phone}")
+    private String phone;
+
+    @Value("${info.app.version}")
+    private String version;
 }
